f4803fbbe2a450b72e8e722c26fb6bddd83a1f50
Marcos Andrade <marcostfandrade@gmail.com>
2024-10-14 23:05:30 -0300

    feat: update

--- angular-app/src/app/estoque/material/material-form/material-form.component.ts
+++ angular-app/src/app/estoque/material/material-form/material-form.component.ts
@@ -2,53 +2,37 @@ import {Component, inject, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MaterialService} from '../material.service';
 import {MatSnackBar} from '@angular/material/snack-bar';
-import {FloatLabelType, MatFormField, MatFormFieldModule, MatLabel} from '@angular/material/form-field';
-import {MatIcon} from '@angular/material/icon';
-import {MatInput} from '@angular/material/input';
+import {MatIcon, MatIconModule} from '@angular/material/icon';
+import {MatInput, MatInputModule} from '@angular/material/input';
 import {MatOption} from '@angular/material/core';
 import {MatRadioButton, MatRadioGroup} from '@angular/material/radio';
 import {MatSelectModule} from '@angular/material/select';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {toSignal} from '@angular/core/rxjs-interop';
-import {map} from 'rxjs';
 import {NgForOf} from '@angular/common';
 import {MatCheckbox} from '@angular/material/checkbox';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-material-form',
   standalone: true,
   imports: [
     FormsModule,
-    MatFormField,
     MatIcon,
     MatInput,
-    MatLabel,
     MatOption,
     MatRadioButton,
     MatRadioGroup,
     MatSlideToggleModule,
     ReactiveFormsModule,
     NgForOf,
-    MatFormFieldModule,
     MatSelectModule,
-    MatCheckbox
+    MatCheckbox,
+    MatFormFieldModule, MatInputModule, MatIconModule
   ],
   templateUrl: './material-form.component.html',
   styleUrl: './material-form.component.scss'
 })
 export class MaterialFormComponent implements OnInit {
-  readonly hideRequiredControl = new FormControl(false);
-  readonly floatLabelControl = new FormControl('auto' as FloatLabelType);
-  readonly options = inject(FormBuilder).group({
-    hideRequired: this.hideRequiredControl,
-    floatLabel: this.floatLabelControl,
-  });
-  protected readonly hideRequired = toSignal(this.hideRequiredControl.valueChanges);
-  protected readonly floatLabel = toSignal(
-    this.floatLabelControl.valueChanges.pipe(map(v => v || 'auto')),
-    {initialValue: 'auto'},
-  );
-
   materialForm: FormGroup;
   tipos: any[] = [];
   grupos: any[] = [];
