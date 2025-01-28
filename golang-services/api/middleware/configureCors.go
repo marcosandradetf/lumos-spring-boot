f5d43fc4ed038986d51dc5760893d7a91405a78f
Marcos Andrade <marcostfandrade@gmail.com>
2025-01-28 19:19:45 -0300

    feat: update cors go

--- golang-services/api/middleware/configureCors.go
+++ golang-services/api/middleware/configureCors.go
@@ -9,15 +9,14 @@ func ConfigureCORS(router *gin.Engine) {
 	// Configuração do middleware CORS
 	router.Use(cors.New(cors.Config{
 		AllowOrigins: []string{
-			"http://localhost:4200",       // Frontend local
-			"https://lumos.thryon.com.br", // Frontend dentro de Docker (se aplicável)
+			"https://lumos.thryon.com.br",
 		},
 		AllowMethods: []string{
 			"GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE",
 		}, // Métodos permitidos
 		AllowHeaders: []string{
 			"Authorization", "Content-Type", "X-Requested-With", "Accept", "Origin", "X-Custom-Header",
-		}, // Permite cabeçalhos necessários, incluindo 'Authorization'
+		},                      // Permite cabeçalhos necessários, incluindo 'Authorization'
 		AllowCredentials: true, // Permite cookies e credenciais
 		MaxAge:           3600, // Cache da preflight request                                                         // Cache da preflight request
 	}))
