85bab0f843aaa9425ba04bbd0c5a5b906917ae27
Marcos Andrade <marcostfandrade@gmail.com>
2025-07-20 16:36:39 -0300

    feat: deploy

--- angular-app/src/app/app-download/app-download.component.ts
+++ angular-app/src/app/app-download/app-download.component.ts
@@ -15,7 +15,7 @@ export class AppDownloadComponent {
 // no seu componente .ts
   downloadApk() {
     const link = document.createElement('a');
-    link.href = 'https://minio.thryon.com.br/apk/com.thryon.apps.android.release_3_2.1.1.apk'; // link do arquivo que quer baixar
+    link.href = 'https://minio.thryon.com.br/apk/com.thryon.apps.android.release_8_2.1.5.apk'; // link do arquivo que quer baixar
     link.download = 'com.thryon.apps.android.release_6_2.1.3.apk'; // nome que o arquivo terá ao baixar
     link.click();
   }
--- springboot-app/src/main/java/com/lumos/lumosspring/update/UpdateController.kt
+++ springboot-app/src/main/java/com/lumos/lumosspring/update/UpdateController.kt
@@ -10,9 +10,9 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api")
 class UpdateController {
 
-    private val latestVersionCode = 7L
-    private val latestVersionName = "2.1.4"
-    private val apkUrl = "https://minio.thryon.com.br/apk/com.thryon.apps.android.release_7_2.1.4.apk"
+    private val latestVersionCode = 8L
+    private val latestVersionName = "2.1.5"
+    private val apkUrl = "https://minio.thryon.com.br/apk/com.thryon.apps.android.release_8_2.1.5.apk"
 
     @GetMapping("/mobile/check-update")
     fun checkUpdate(@RequestParam version: Long): ResponseEntity<Any> {
